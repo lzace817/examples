9b85dd9e55d519ba92edda6837254a76836851d0
proto <proto.x@hotmail.com>
2025-11-14 23:46:00 -0300

    use same default permissions as fopen

--- hello-world/low-level-file/build.sh
+++ hello-world/low-level-file/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-mkdir -p build
 gcc hello.c -o hello
 
 echo -e "\033[1;32mDONE!\033[0m"
--- hello-world/low-level-file/hello.c
+++ hello-world/low-level-file/hello.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    int fd = open("out.txt", O_CREAT | O_WRONLY, 0664);
+    int fd = open("out.txt", O_CREAT | O_WRONLY, 0666);
     write(fd, "Hello world with low level file api\n", 36);
     close(fd);
     return 0;
